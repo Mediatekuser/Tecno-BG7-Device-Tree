530285dd65bae948ca565a0a343b1e17de47169e
Mediatekuser <estrogajohnrhys@gmail.com>
2026-05-29 10:12:31 +0000

    Fixed missing system.prop and corrected BootControl casing

--- twrp_BG7.mk
+++ twrp_BG7.mk
@@ -13,7 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from TECNO-BG7 device
-$(call inherit-product, device/tecno/TECNO-BG7/device.mk)
+$(call inherit-product, device/tecno/BG7/device.mk)
 
 PRODUCT_DEVICE := BG7
 PRODUCT_NAME := twrp_BG7
