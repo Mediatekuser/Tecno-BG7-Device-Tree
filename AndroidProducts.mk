8298edaad766c84fad39caa8632aa3e8178bac7d
Mediatekuser <estrogajohnrhys@gmail.com>
2026-05-29 14:54:09 +0800

    Restoring deleted TWRP device tree

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -7,10 +7,10 @@
 
 # Reference your custom makefile
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_Bg7.mk
+    $(LOCAL_DIR)/twrp_BG7.mk
 
 # Customize lunch choices based on your needs
 COMMON_LUNCH_CHOICES := \
-    twrp_Bg7-user \
-    twrp_Bg7-userdebug \
-    twrp_Bg7-eng
+    twrp_BG7-user \
+    twrp_BG7-userdebug \
+    twrp_BG7-eng
--- twrp_bg7.mk
+++ twrp_bg7.mk
@@ -9,18 +9,18 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from TECNO-BG7 device
 $(call inherit-product, device/tecno/TECNO-BG7/device.mk)
 
-PRODUCT_DEVICE := TECNO-BG7
-PRODUCT_NAME := omni_TECNO-BG7
+PRODUCT_DEVICE := BG7
+PRODUCT_NAME := twrp_BG7
 PRODUCT_BRAND := TECNO
 PRODUCT_MODEL := TECNO BG7
 PRODUCT_MANUFACTURER := tecno
 
 PRODUCT_GMS_CLIENTID_BASE := android-tecno
 
-BUILD_FINGERPRINT := TECNO/LH8n-GL/TECNO-LH8n:12/SP1A.210812.016/240703V513:user/release-keys
+BUILD_FINGERPRINT := TECNO/BG7-OPPJ/TECNO-BG7:12/SP1A.210812.016/260121V152:user/release-keys
