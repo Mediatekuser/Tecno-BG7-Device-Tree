59db474640ee57e5dc149c78afdef61aa96c62e9
Mediatekuser <estrogajohnrhys@gmail.com>
2026-06-01 23:26:17 +0800

    Fix PRODUCT_NAME formatting in omni_TECNO_BG7.mk

--- omni_TECNO_BG7.mk
+++ omni_TECNO_BG7.mk
@@ -16,7 +16,7 @@ $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, device/tecno/TECNO-BG7/device.mk)
 
 PRODUCT_DEVICE := TECNO-BG7
-PRODUCT_NAME := omni_TECNO-BG7
+PRODUCT_NAME := omni_TECNO_BG7
 PRODUCT_BRAND := TECNO
 PRODUCT_MODEL := TECNO BG7
 PRODUCT_MANUFACTURER := tecno
