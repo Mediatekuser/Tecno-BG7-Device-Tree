b830a61147608d2c58ef1497ef02780f9587c242
Mediatekuser <estrogajohnrhys@gmail.com>
2026-06-01 23:26:59 +0800

    Fix product name formatting in AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_TECNO-BG7.mk
+    $(LOCAL_DIR)/omni_TECNO_BG7.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_TECNO-BG7-user \
-    omni_TECNO-BG7-userdebug \
-    omni_TECNO-BG7-eng
+    omni_TECNO_BG7-user \
+    omni_TECNO_BG7-userdebug \
+    omni_TECNO_BG7-eng
